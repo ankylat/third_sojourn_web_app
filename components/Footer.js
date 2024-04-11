2e498850890d98444204794d49ccffb56ee6c0b5
jp <jp@jps-MacBook-Air.local>
2024-04-10 23:23:17 -0400

    aloja

--- components/Footer.js
+++ components/Footer.js
@@ -4,39 +4,45 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <section className="px-2 md:px-16 h-fit md:h-72 bg-purple-400 w-full flex justify-around py-8 justify-between flex-col md:flex-row ">
-      <div className="flex flex-col w-fit">
-        <Link href="/" className="w-32 h-16 relative cursor-pointer" passHref>
-          <Image src="/images/anky-logo.png" fill />
-        </Link>
-        <div className="text-xl mt-4">© 2024</div>
-        <div className="text-xl mt-4">
-          <Link href="/terms-and-conditions">Privacy - Terms</Link>
+    <div className="flex flex-col w-full">
+      <section className="px-2 md:px-16 h-fit md:h-72 bg-purple-400 w-full flex justify-around py-8 justify-between flex-col md:flex-row ">
+        <div className="flex flex-col w-fit">
+          <Link href="/" className="w-32 h-16 relative cursor-pointer" passHref>
+            <Image src="/images/anky-logo.png" fill />
+          </Link>
+          <div className="text-xl mt-4">© 2024</div>
+          <div className="text-xl mt-4">
+            <Link href="/terms-and-conditions">Privacy - Terms</Link>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col w-fit">
-        <div className="text-xl mt-4">
-          <Link href="/product">Product</Link>
+        <div className="flex flex-col w-fit">
+          <div className="text-xl mt-4">
+            <Link href="/product">Product</Link>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col w-fit">
-        <div className="text-xl mt-4">
-          <Link href="/features">Features</Link>
+        <div className="flex flex-col w-fit">
+          <div className="text-xl mt-4">
+            <Link href="/features">Features</Link>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col w-fit">
-        <div className="text-xl mt-4">
-          <Link href="/newen">$newen</Link>
+        <div className="flex flex-col w-fit">
+          <div className="text-xl mt-4">
+            <Link href="/newen">$newen</Link>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col w-fit">
-        <div className="text-xl mt-4">
-          <a href="https://t.me/ankytheape" target="_blank">
-            telegram
-          </a>
+        <div className="flex flex-col w-fit">
+          <div className="text-xl mt-4">
+            <a href="https://t.me/ankytheape" target="_blank">
+              telegram
+            </a>
+          </div>
         </div>
+      </section>
+      <div className="bg-black text-white flex flex-col items-center justify-center py-8">
+        <p className="text-4xl">JUST WRITE</p>
+        <p>we will do the rest</p>
       </div>
-    </section>
+    </div>
   );
 };
 
